691d1d799bfc60b62e1b7137125a941f58437c8a
Iury Gontijo Silva <iurygon.silva@gmail.com>
2024-02-07 23:47:19 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import chalk from 'chalk'   /*IMPORTA A BIBLIOTECA CHALK, USADA PARA ESTILIZAR CORES NO TERMINAL */
 import fs from 'fs'         /*IMPORTA A BIBLIOTECA FS, FILE SYSTEM, PARA ACESSAR ARQUIVOS EXTERNOS*/
 
+function trataErro(erro){
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório.'));
+};
+
 function pegaArquivo(caminhoArquivo){
     const enconding = 'utf-8';
-    fs.readFile(caminhoArquivo, enconding,  (_, texto) => { /*MÉTODO PRÓPRIO DA BIBLIOTECA FS*/
+    fs.readFile(caminhoArquivo, enconding,  (erro, texto) => { /*MÉTODO PRÓPRIO DA BIBLIOTECA FS*/
+        if(erro){
+            trataErro(erro)
+        };
         console.log(chalk.green(texto));
+
     });
 };
 
